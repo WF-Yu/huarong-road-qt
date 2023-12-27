84911a3dbf96bcabc7b90c43b59dd0600f6d8199
Wenfei Yu <ywfywf2019@163.com>
2023-12-27 11:00:05 +0100

    added keyboard functions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,10 +8,17 @@
 #include <QInputDialog>
 #include <QMouseEvent>
 #include <QDebug>
+#include <QCoreApplication>
+#include <QtMultimedia/QMediaPlaylist>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QMessageBox>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent),
       mainBoard(5, 1)
 {
+    // 设置窗口的初始大小为固定值
+    resize(800, 800);
     // Create the main view
     view = new QGraphicsView(this);
     setCentralWidget(view);
@@ -25,11 +32,43 @@ MainWindow::MainWindow(QWidget *parent)
     qDebug() << "hi";
     // Create the initial grid with default value.
     mainBoard.generateRandomCars();
+    keyBoardRow = 0;
+    keyBoardColumn = 0;
     createGrid();
 
+   // 设置背景音乐文件路径
+   // QString musicFilePath = "F:/MyProject/Qt_projects/huarong-road-qt/LOL.mp3";
 
     // Generate menu
     generateMenu();
+
+    generatePromptMsg();
+
+}
+void MainWindow::generatePromptMsg() {
+    QVBoxLayout *centralLayout = new QVBoxLayout(centralWidget());
+
+    // Add a spacer item to the right to create some space
+    // centralLayout->addItem(new QSpacerItem(200, 400, QSizePolicy::Expanding, QSizePolicy::Minimum));
+
+    // Create and add the label to the layout
+    QLabel *label = new QLabel(
+        "汽车华容道：将目标车右移，移动到窗口最右侧（当目标车右侧没有车辆阻拦时，即为通关）\n"
+        "绿色：键盘当前位置\n"
+        "红色：目标车\n"
+        "黄色：长度为2的车\n"
+        "蓝色：长度为3的车\n"
+        "键盘操作：\n"
+        "    wsad键：上下左右\n"
+        "    ctrl+wsad：将键盘所在位置的车向上下左右方向移动\n"
+        , this);
+
+    label->setAlignment(Qt::AlignLeft); // 设置对齐方式，根据需要调整
+    centralLayout->addWidget(label);
+
+    // Set the layout for the central widget
+    centralWidget()->setLayout(centralLayout);
+
 }
 
 void MainWindow::createGrid()
@@ -94,7 +133,12 @@ void MainWindow::createGrid()
         for (int j = 0; j < N; ++j) { // x
             QGraphicsRectItem *rect = new QGraphicsRectItem(j * squareSize, i * squareSize, squareSize, squareSize);
             if (j == rectRow && i == rectColumn) {
+                // instructions: which rect to click next
                 rect->setBrush(QColor(Qt::black));
+            }
+            else if (j == keyBoardRow && i == keyBoardColumn) {
+                // current keyboard position
+                rect->setBrush(QColor(Qt::green));
             }
             else if (mainBoard.getCarID(j,i) == 99) {
                 rect->setBrush(QColor(Qt::red));
@@ -181,6 +225,134 @@ void MainWindow::mousePressEvent(QMouseEvent *event)
     QMainWindow::mousePressEvent(event);
 }
 
+void MainWindow::keyPressEvent(QKeyEvent *event) {
+   if (event->modifiers() == Qt::ControlModifier && event->key() == Qt::Key_A) {
+       qDebug() << "Ctrl+A Pressed";
+       handKeyPress_CtrlA();
+   }
+   else if (event->modifiers() == Qt::ControlModifier && event->key() == Qt::Key_W) {
+       qDebug() << "Ctrl+W Pressed";
+       handKeyPress_CtrlW();
+   }
+   else if (event->modifiers() == Qt::ControlModifier && event->key() == Qt::Key_D) {
+       qDebug() << "Ctrl+D Pressed";
+       handKeyPress_CtrlD();
+   }
+   else if (event->modifiers() == Qt::ControlModifier && event->key() == Qt::Key_S) {
+       qDebug() << "Ctrl+S Pressed";
+       handKeyPress_CtrlS();
+   }
+   else if (event->key() == Qt::Key_A) {
+       qDebug() << "A Pressed";
+       handKeyPress_A();
+   }
+   else if (event->key() == Qt::Key_W) {
+       qDebug() << "W Pressed";
+       handKeyPress_W();
+   }
+   else if (event->key() == Qt::Key_D) {
+       qDebug() << "D Pressed";
+       handKeyPress_D();
+   }
+   else if (event->key() == Qt::Key_S) {
+       qDebug() << "S Pressed";
+       handKeyPress_S();
+   }
+   // 调用基类的函数以确保其他键盘事件正常处理
+   QWidget::keyPressEvent(event);
+}
+void MainWindow::handKeyPress_CtrlA() { // move to left
+    Car* car = mainBoard.getCar(keyBoardRow, keyBoardColumn);
+    if (car == nullptr) return;
+    if (car->direction == 2) {
+        if (car->row > 0) {
+            car->row -= 1;
+        }
+    }
+    mainBoard.update(); // update new car position to board
+    if (mainBoard.cleared()) {
+        // succeed!
+        ShowSuccessInfo();
+        ifShowInstructions = false;
+    }
+    createGrid(); // draw new graph
+
+}
+void MainWindow::handKeyPress_CtrlW() { // move up
+    Car* car = mainBoard.getCar(keyBoardRow, keyBoardColumn);
+    if (car == nullptr) return;
+
+    if (car->direction == 1) {
+        if (car->column > 0) {
+            car->column -= 1;
+        }
+    }
+    mainBoard.update(); // update new car position to board
+    if (mainBoard.cleared()) {
+        // succeed!
+        ShowSuccessInfo();
+        ifShowInstructions = false;
+    }
+    createGrid(); // draw new graph
+}
+void MainWindow::handKeyPress_CtrlD() { // move left
+    Car* car = mainBoard.getCar(keyBoardRow, keyBoardColumn);
+    if (car == nullptr) return;
+
+    if (car->direction == 2) {
+        if (car->row + car->length < mainBoard.size) {
+            car->row += 1;
+        }
+    }
+    mainBoard.update(); // update new car position to board
+    if (mainBoard.cleared()) {
+        // succeed!
+        ShowSuccessInfo();
+        ifShowInstructions = false;
+    }
+    createGrid(); // draw new graph
+}
+void MainWindow::handKeyPress_CtrlS() { // move down
+    Car* car = mainBoard.getCar(keyBoardRow, keyBoardColumn);
+    if (car == nullptr) return;
+
+    if (car->direction == 1) {
+        if (car->column + car->length < mainBoard.size) {
+            car->column += 1;
+        }
+    }
+    mainBoard.update(); // update new car position to board
+    if (mainBoard.cleared()) {
+        // succeed!
+        ShowSuccessInfo();
+        ifShowInstructions = false;
+    }
+    createGrid(); // draw new graph
+}
+void MainWindow::handKeyPress_A() { // move left
+    if (keyBoardRow > 0) {
+        keyBoardRow -= 1;
+        createGrid(); // draw new graph
+    }
+}
+void MainWindow::handKeyPress_W() { // move up
+    if (keyBoardColumn > 0) {
+        keyBoardColumn -= 1;
+        createGrid(); // draw new graph
+    }
+}
+void MainWindow::handKeyPress_D() { // move right
+    if (keyBoardRow < mainBoard.size - 1) {
+        keyBoardRow += 1;
+        createGrid(); // draw new graph
+    }
+}
+void MainWindow::handKeyPress_S() { // move down
+    if (keyBoardColumn < mainBoard.size - 1) {
+        keyBoardColumn += 1;
+        createGrid(); // draw new graph
+    }
+}
 void MainWindow::handleLeftMouseClick(const QPointF& clickPos)
 {
 
@@ -189,6 +361,8 @@ void MainWindow::handleLeftMouseClick(const QPointF& clickPos)
     int squareSize = 50;
     int clickedX = static_cast<int>(clickPos.x()) / squareSize;
     int clickedY = (static_cast<int>(clickPos.y()) - 30) / squareSize;
+    setKeyboard(clickedX, clickedY);
+
     Car* car = mainBoard.getCar(clickedX, clickedY);
 
     if (car != nullptr) {
@@ -213,6 +387,7 @@ void MainWindow::handleLeftMouseClick(const QPointF& clickPos)
         mainBoard.update();
         if (mainBoard.cleared()) {
             // succeed!
+            ShowSuccessInfo();
             ifShowInstructions = false;
         }
         createGrid(); // draw new graph
@@ -226,6 +401,8 @@ void MainWindow::handleRightMouseClick(const QPointF& clickPos)
     int squareSize = 50;
     int clickedX = static_cast<int>(clickPos.x()) / squareSize;
     int clickedY = (static_cast<int>(clickPos.y()) - 30) / squareSize;
+    setKeyboard(clickedX, clickedY);
+
     Car* car = mainBoard.getCar(clickedX, clickedY);
     if (car != nullptr) {
         // test:
@@ -250,12 +427,31 @@ void MainWindow::handleRightMouseClick(const QPointF& clickPos)
         mainBoard.update();
         if (mainBoard.cleared()) {
             // succeed!
-            // show a windows and tell use that he succeed. don't exit. use has to start a new game by himself
+            ShowSuccessInfo();
             ifShowInstructions = false;
         }
         createGrid(); // draw new graph
     }
 
 }
+void MainWindow::ShowSuccessInfo() {
+    // 创建 QMessageBox 实例
+    QMessageBox msgBox;
 
+    // 设置提示框的标题
+    msgBox.setWindowTitle("提示");
 
+    // 设置提示框的文本信息
+    msgBox.setText("闯关成功！开始新游戏");
+
+    // 设置提示框的图标（例如：QMessageBox::Information、QMessageBox::Warning等）
+    msgBox.setIcon(QMessageBox::Information);
+
+    // 添加按钮，例如：Ok、Cancel等
+    msgBox.addButton(QMessageBox::Ok);
+
+    // 显示提示框，并等待用户响应
+    msgBox.exec();
+
+    newGame();
+}
--- mainwindow.h
+++ mainwindow.h
@@ -4,6 +4,8 @@
 #include <QGraphicsView>
 #include <QGraphicsScene>
 #include <QComboBox>
+#include <QtMultimedia/QMediaPlayer>
+
 #include "board.h"
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -18,13 +20,35 @@ public:
     void mousePressEvent(QMouseEvent *event);
     void handleLeftMouseClick(const QPointF& clickPos);
     void handleRightMouseClick(const QPointF& clickPos);
+
+    void keyPressEvent(QKeyEvent *event);
+    void handKeyPress_CtrlA();
+    void handKeyPress_CtrlW();
+    void handKeyPress_CtrlD();
+    void handKeyPress_CtrlS();
+    void handKeyPress_A();
+    void handKeyPress_W();
+    void handKeyPress_D();
+    void handKeyPress_S();
+
     bool ifShowInstructions;
+    void setKeyboard(int x, int y) {
+        keyBoardRow = x;
+        keyBoardColumn = y;
+    }
+
+
 private slots:
     void newGame();
     void instructions();
 private:
     void createGrid();
     void generateMenu();
+    void generatePromptMsg();
+    void ShowSuccessInfo();
+
+    int keyBoardRow;
+    int keyBoardColumn;
 
     Board mainBoard; // board to show
 
@@ -32,6 +56,8 @@ private:
     QGraphicsView *view;
     QVector<QVector<QGraphicsRectItem*>> gridCells;
     QComboBox *difficultyComboBox; // Added ComboBox for difficulty selection
+
+    QMediaPlayer *backgroundMusicPlayer;
 };
 
 #endif // MAINWINDOW_H
